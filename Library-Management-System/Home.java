160e38a135a8a8b90e272ae8e14277e951c539e6
Jai Gora <Jaigora24@gmail.com>
2022-01-17 14:25:45 +0530

    Update Home.java

--- Library-Management-System/Home.java
+++ Library-Management-System/Home.java
@@ -16,9 +16,7 @@ public class Home extends JFrame {
 
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -32,9 +30,7 @@ public class Home extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+
 	public Home() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 446);
